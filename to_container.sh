a7cfa0bcd63acf8a9454c589239d568bbeb7f1c5
root <root@vps-7db540e3.vps.ovh.net>
2021-01-12 03:06:51 +0000

    Use bloody docker cp

--- to_container.sh
+++ to_container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # https://medium.com/@gchudnov/copying-data-between-docker-containers-26890935da3f
-docker exec -i docker_backend_1 sh -c 'cat > /root/dump-product.pl' < ./dump-product.pl
+# docker exec -i docker_backend_1 sh -c 'cat > /root/dump-product.pl' < ./dump-product.pl
+docker cp ./dump-product.pl docker_backend_1:/root
